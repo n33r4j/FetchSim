da315ada8cb8e2181fcfa73fae6e25aa9a9bfa0b
n334j <n33r4j@users.noreply.github.com>
2022-11-15 05:38:42 +1100

    Add base movement commands

--- src/fetch_move_v1/basic_command_publisher.py
+++ src/fetch_move_v1/basic_command_publisher.py
@@ -43,6 +43,7 @@ from control_msgs.msg import (FollowJointTrajectoryAction,
                               GripperCommandGoal)
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from std_msgs.msg import Int32MultiArray, Float32MultiArray
+from geometry_msgs.msg import PointStamped, Point, Twist
 import time
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
@@ -72,11 +73,24 @@ rate_error = 0.0
 prev_time = 0.0
 check_interval = 0.5 # sec
 
-actor_position = [0.0, 0.0, 2.0]
+prev_actor_position = Point(x=-1000.0,y=0.0,z=0.0)
 
 soundhandle = SoundClient()
 voice = 'voice_kal_diphone'
 volume = 1.0
+
+# base_pub = rospy.Publisher('base_controller/command', Twist, queue_size=10)
+
+MIN_LIN_VEL = -1.0
+MAX_LIN_VEL = 1.0
+MIN_ANG_VEL = -0.5
+MAX_ANG_VEL = 0.5
+
+# def Dist3D(p1, p2):
+#     dist = 0.0
+#     for i in range(3):
+#         dist += ((p2[i]-p1[i])**2)
+#     return dist
 
 # Point the head using controller
 class PointHeadClient(object):
@@ -128,90 +142,148 @@ def MoveHead(goal_time, pan = 0.0, tilt = 0.0):
     head_client.send_goal(head_goal)
     head_client.wait_for_result(rospy.Duration(goal_time + 2.0))
 
-def position_callback(data):
-    global actor_position
-    actor_position = data
+class CommandHandler:
+    def __init__(self):
+        rospy.init_node("basic_command_publisher")
+        self.body_pose_sub = rospy.Subscriber('body25_pose_data', Float32MultiArray, self.body_pose_callback)
+        self.human_pos_sub =  rospy.Subscriber('human_pos', PointStamped, self.human_position_callback)
+        self.base_command_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.twist_msg = Twist()
+        self.twist_msg.linear.x = 0.0
+        self.twist_msg.linear.y = 0.0
+        self.twist_msg.linear.z = 0.0
+        self.twist_msg.angular.x = 0.0
+        self.twist_msg.angular.y = 0.0
+        self.twist_msg.angular.z = 0.0
 
-def callback(data):
-    global human_Xpos
-    global curr_pan_goal
-    global prev_time
-    global curr_error, last_error, cum_error, rate_error
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+        self.actor_position = Point()
+        self.actor_position.x = 0.0
+        self.actor_position.y = 0.0
+        self.actor_position.z = 0.0
 
-    # Get Keypoints data
-    LShoulder_kp = [data.data[0], data.data[1]]
-    Neck_kp = [data.data[2], data.data[3]]
-    RShoulder_kp = [data.data[4], data.data[5]]
-    
-    # Calculate Human X Position (Setpoint)
-    human_Xpos = (LShoulder_kp[0] + Neck_kp[0] + RShoulder_kp[0])// 3
-    # rospy.loginfo(f"[[ human_pos: {human_Xpos} ]]")
-    # head_action.look_at(0.0, 0.0, 0.0, 'base_link/torso_lift_link/head_pan_link/head_tilt_link/head_camera_link/head_camera_rgb_frame/human_actor')
-    # head_action.look_at(actor_position[0], actor_position[1], actor_position[2], 'head_camera_rgb_frame')
-    # curr_time = time.time()
-    # dt = (curr_time - prev_time)
-    # curr_error = human_Xpos - 320.0
-    # cum_error += (curr_error*dt)
-    # rate_error = (curr_error - last_error)/dt
+    def publish_base_command(self):
+        self.base_command_pub.publish(self.twist_msg)
+        rospy.loginfo("Publishing cmd_vel commands...")
+        self.stop_moving()
 
-    # curr_pan_goal = (Kp*curr_error)+(Kd*rate_error)
-    curr_pan_goal = (human_Xpos-320.0)/320.0
-    curr_pan_goal = max(-1.2, min(curr_pan_goal, 1.2))
-    rospy.loginfo(f"goal pos: {curr_pan_goal}")
+    def stop_moving(self):
+        self.twist_msg.linear.x = 0.0
+        self.twist_msg.angular.z = 0.0
+        rospy.loginfo("Stoppping...")
 
-    # last_error = curr_error
-    # prev_time = curr_time
+    def move_forward(self):
+        self.twist_msg.linear.x = 1.0
+        self.twist_msg.angular.z = 0.0
+        rospy.loginfo("Moving forward...")
 
-    # MoveHead(0.001, curr_pan_goal, 0.0)
-    
-    # rospy.loginfo(f"dt: {(time.time())}")
-    # if (time.time()-prev_time) > check_interval:
-        # p_gain = 0.05 + 0.4*abs(human_Xpos - 320.0)/320.0
-        # rospy.loginfo(f"gain: {p_gain}")
-        # if human_Xpos < 290.0:
-        #     rospy.loginfo("Moving head left...")
-        #     curr_pan_goal = head_joint_positions[0]+p_gain
-        #     # IS_MOVING = False
+    def move_right():
+        #TODO
+        pass
 
-        # elif human_Xpos > 330.0:
-        #     rospy.loginfo("Moving head right...")
-        #     curr_pan_goal = head_joint_positions[0]-p_gain
-        #     # IS_MOVING = False
-        # curr_pan_goal = (human_Xpos-320)/320.0 - head_joint_positions[0]
-        # if p_gain != 0.0:
-    MoveHead(0.0001, curr_pan_goal, 0.0)
-    
-        # prev_time = time.time()
-    # return data.data
-    
-def listener():
+    def move_left():
+        #TODO
+        pass
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    
+    def rotate_body(self, direction):
+        self.twist_msg.linear.x = 0.0
+        self.twist_msg.angular.z = 0.4 * (-1.0 if direction=="right" else 1.0)
+        rospy.loginfo(f"Rotating {direction}...")
 
-    # rospy.Subscriber('pose_data', Int32MultiArray, callback)
-    rospy.Subscriber('pose_data', Int32MultiArray, callback)
-    rospy.Subscriber('actor_position', Float32MultiArray, position_callback)
+    def human_position_callback(self, data):
+        # global actor_position
+        global curr_pan_goal
+        rospy.loginfo(rospy.get_caller_id() + "I heard human position data")
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-    rospy.loginfo("[ inside listener ]")
+        y_speed_limit = 0.1
+        z_speed_limit = 0.1
+        pan_speed = 0.00001
+
+        if prev_actor_position.x != -1000.0:
+            # We won't track fast moving objects
+            if abs(prev_actor_position.y-data.point.y) < y_speed_limit and abs(prev_actor_position.z-data.point.z) < z_speed_limit:
+                actor_position = data.point
+                # head_action.look_at(2.0, actor_position.y, 0.0, 'head_camera_rgb_frame')
+                if actor_position.y > 0.0:
+                    curr_pan_goal += pan_speed*abs(actor_position.y)
+                elif actor_position.y < 0.0:
+                    curr_pan_goal -= pan_speed*abs(actor_position.y)
+                MoveHead(0.001, curr_pan_goal, 0.0)
+            
+        prev_actor_position.x = data.point.x
+        prev_actor_position.y = data.point.y
+        prev_actor_position.z = data.point.z
+        # head_action.look_at(actor_position.x, actor_position.y, actor_position.z, 'odom')
+
+    def body_pose_callback(self, data):
+        global human_Xpos
+        global curr_pan_goal
+        global prev_time
+        global curr_error, last_error, cum_error, rate_error
+        rospy.loginfo(rospy.get_caller_id() + "I heard pose data")
+        
+        # Simply putting this here will cause buffering issues.
+        # soundhandle.say("I see a human", voice, volume)
+
+        # Get Keypoints data
+        LShoulder_kp = [data.data[4], data.data[5]]
+        Neck_kp = [data.data[2], data.data[3]]
+        RShoulder_kp = [data.data[10], data.data[11]]
+        
+        # Calculate Human X Position (Setpoint)
+        human_Xpos = (LShoulder_kp[0] + Neck_kp[0] + RShoulder_kp[0])// 3
+        # rospy.loginfo(f"[[ human_pos: {human_Xpos} ]]")
+        
+        # head_action.look_at(0.0, 0.0, 0.0, 'base_link/torso_lift_link/head_pan_link/head_tilt_link/head_camera_link/head_camera_rgb_frame/human_actor')
+        # head_action.look_at(actor_position[0], actor_position[1], actor_position[2], 'head_camera_rgb_frame')
+        
+        # curr_time = time.time()
+        # dt = (curr_time - prev_time)
+        # curr_error = human_Xpos - 320.0
+        # cum_error += (curr_error*dt)
+        # rate_error = (curr_error - last_error)/dt
+
+        # curr_pan_goal = (Kp*curr_error)+(Kd*rate_error)
+        curr_pan_goal = (human_Xpos-320.0)/320.0
+        curr_pan_goal = max(-1.2, min(curr_pan_goal, 1.2))
+        rospy.loginfo(f"goal pos: {curr_pan_goal}")
+
+        # last_error = curr_error
+        # prev_time = curr_time
+
+        # MoveHead(0.001, curr_pan_goal, 0.0)
+        
+        # rospy.loginfo(f"dt: {(time.time())}")
+        # if (time.time()-prev_time) > check_interval:
+            # p_gain = 0.05 + 0.4*abs(human_Xpos - 320.0)/320.0
+            # rospy.loginfo(f"gain: {p_gain}")
+            # if human_Xpos < 290.0:
+            #     rospy.loginfo("Moving head left...")
+            #     curr_pan_goal = head_joint_positions[0]+p_gain
+            #     # IS_MOVING = False
+
+            # elif human_Xpos > 330.0:
+            #     rospy.loginfo("Moving head right...")
+            #     curr_pan_goal = head_joint_positions[0]-p_gain
+            #     # IS_MOVING = False
+            # curr_pan_goal = (human_Xpos-320)/320.0 - head_joint_positions[0]
+            # if p_gain != 0.0:
+        # MoveHead(0.0001, curr_pan_goal, 0.0)
+        
+            # prev_time = time.time()
+        # return data.data
+
 
 
 if __name__ == "__main__":
     
-    rospy.init_node("basic_command_publisher")
+    
 
     # ALWAYS CHECK YOUR CODE IN SIMULATION BEFORE RUNNING IT ON THE REAL ROBOT.
     # Check robot serial number, we never want to run this on a real robot!
     # if rospy.get_param("robot/serial") != "ABCDEFGHIJKLMNOPQRSTUVWX":
     #     rospy.logerr("This script should not be run on a real robot")
     #     sys.exit(-1)
+    CH = CommandHandler()
 
     rospy.loginfo("Waiting for head_controller...")
     head_client = actionlib.SimpleActionClient("head_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
@@ -230,10 +302,38 @@ if __name__ == "__main__":
     # gripper_client.wait_for_server()
     # rospy.loginfo("...connected.")
     
-    soundhandle.say("Moving to home position", voice, volume)
-    MoveHead(1.0, 0.0, 0.0)
+    # Put say calls before Move()
 
-    listener()
+    # soundhandle.say("Moving to home position", voice, volume)
+    MoveHead(1.0, 0.0, 0.0)
+    rospy.sleep(1.0)
+    
+    # soundhandle.say("Looking Right", voice, volume)
+    # MoveHead(1.0, -1.0, 0.0)
+    # rospy.sleep(1.0)
+    
+    # head_action.look_at(1.0, 1.0, 0.0, 'head_camera_rgb_frame')
+
+    # soundhandle.say("Looking Left", voice, volume)
+    # MoveHead(1.0, 1.0, 0.0)
+    # rospy.sleep(1.0)
+
+    # soundhandle.say("Moving to home position", voice, volume)
+    # MoveHead(1.0, 0.0, 0.0)
+    # rospy.sleep(1.0)
+
+    # rospy.Time.now()
+    try:
+        while not rospy.is_shutdown():
+            if human_Xpos > 320.0:
+                CH.rotate_body("right")
+            elif human_Xpos < 300.0:
+                CH.rotate_body("left")
+
+            CH.publish_base_command()
+    except KeyboardInterrupt:
+        rospy.signal_shutdown()
+
 
     # MoveHead is absolute i.e. move to specified absolute angle.
     # MoveHead(1.0, 0.0, 0.0)
